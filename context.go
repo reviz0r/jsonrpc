bf376291dbdd6c50491ace3a2c8dba635d027c36
Nikita Kurochkin <n.kurochkin@postgrespro.ru>
2026-03-02 17:44:52 +0500

    Rework context values

--- context.go
+++ context.go
@@ -9,9 +9,13 @@ const requestID contextKey = "request_id"
 // RequestID takes request id from context
 func RequestID(ctx context.Context) string {
 	raw := ctx.Value(requestID)
-	value, ok := raw.(*id)
+	value, ok := raw.(id)
 	if !ok {
 		return ""
 	}
 	return value.String()
+}
+
+func requestIDToContext(ctx context.Context, reqID id) context.Context {
+	return context.WithValue(ctx, requestID, reqID)
 }
