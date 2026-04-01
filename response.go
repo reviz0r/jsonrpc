5cc54773a54043fde6dfc8d36aa5b4f66c0a92c6
Nikita Kurochkin <n.kurochkin@postgrespro.ru>
2026-04-01 15:19:49 +0500

    Fix typo

--- response.go
+++ response.go
@@ -5,13 +5,13 @@ import "encoding/json"
 // response represents a JSON-RPC response returned by the server
 type response struct {
 	ID      *ID             `json:"id"`
-	Jsonprc string          `json:"jsonrpc"`
+	Jsonrpc string          `json:"jsonrpc"`
 	Result  json.RawMessage `json:"result,omitempty"`
 	Error   *Error          `json:"error,omitempty"`
 }
 
 func responseWithResult(id *ID, result json.RawMessage) (json.RawMessage, error) {
-	return json.Marshal(response{ID: id, Jsonprc: jsonrpcVersion, Result: result})
+	return json.Marshal(response{ID: id, Jsonrpc: jsonrpcVersion, Result: result})
 }
 
 func responseWithError(id *ID, isNotification bool, err *Error) (json.RawMessage, error) {
@@ -19,5 +19,5 @@ func responseWithError(id *ID, isNotification bool, err *Error) (json.RawMessage
 		return nil, nil
 	}
 
-	return json.Marshal(response{ID: id, Jsonprc: jsonrpcVersion, Error: err})
+	return json.Marshal(response{ID: id, Jsonrpc: jsonrpcVersion, Error: err})
 }
